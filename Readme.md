6f19a6be060299c1e92220327ee3e12949c23610
FloatinggOnion <jesseosems123@gmail.com>
2023-11-17 08:59:34 +0100

    Add readme; Update env varibles

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# CEN 434 Projects
+
+*Author: Osemeke Jesse-Paul Miracle 20CJ027483*
+
+## About
+- This is a collection of projects worked on over the semester in the CEN 434 (Computer Software Engineering) class.
--- python/mysql_crud.py
+++ python/mysql_crud.py
@@ -1,24 +1,27 @@
+import os
+
 import mysql.connector as conn
 
 my_db = conn.connect(
-    host = 'localhost',
-    user='username',
-    password='password'
+    host = os.environ.get('DB_HOST'),
+    user = os.environ.get('DB_USER'),
+    password = os.environ.get('DB_PASSWORD')
 )
 
-print(my_db)
+# print(my_db)
 
 my_cursor = my_db.cursor()
 
 # Create a database
-my_cursor.execute('CREATE DATABASE cen434')
+# my_cursor.execute('CREATE DATABASE cen434')
+my_cursor.execute('USE cen434')
 
 # Create table
 my_cursor.execute("CREATE TABLE students (name VARCHAR(255), matno VARCHAR(255))")
 
-# Add info
-sql = "INSERT INTO customers (name, matno) VALUES (%s, %s)"
-val = ("John", "20cj027xxx")
+# Add data
+sql = "INSERT INTO students (name, matno) VALUES (%s, %s)"
+val = ("Kathy", "20cj027xxx")
 my_cursor.execute(sql, val)
 
 my_db.commit()
@@ -33,34 +36,34 @@ myresult = my_cursor.fetchall()
 for x in myresult:
   print(x)
 
-# Delete data
-sql = "DELETE FROM students WHERE matno = '20cj027xxx'"
+# # Update data
+# sql = "UPDATE students SET matno = '20CJ028xxx' WHERE address = '20CJ027xxx'"
 
-my_cursor.execute(sql)
+# my_cursor.execute(sql)
 
-my_db.commit()
+# my_db.commit()
 
-print(my_cursor.rowcount, "record(s) deleted")
+# print(my_cursor.rowcount, "record(s) affected")
 
-# Delete table
-sql = "DROP TABLE students"
+# # Delete data
+# sql = "DELETE FROM students WHERE matno = '20cj027xxx'"
 
-my_cursor.execute(sql)
+# my_cursor.execute(sql)
 
-# Delete database
-sql = "DROP DATABASE cen434"
+# my_db.commit()
 
-my_cursor.execute(sql)
+# print(my_cursor.rowcount, "record(s) deleted")
+
+# # Delete table
+# sql = "DROP TABLE students"
+
+# my_cursor.execute(sql)
+
+# # Delete database
+# sql = "DROP DATABASE cen434"
+
+# my_cursor.execute(sql)
 
 
-# Update data
-sql = "UPDATE students SET matno = '20CJ028xxx' WHERE address = '20CJ027xxx'"
-
-my_cursor.execute(sql)
-
-my_db.commit()
-
-print(my_cursor.rowcount, "record(s) affected")
-
-# close connection
-my_db.close()
+# # close connection
+# my_db.close()
